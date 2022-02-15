6328604b11f1f6c796fb0b5538f239899c1e9955
Serhii Holub <serhii.holub@valtech.com>
2022-02-15 16:19:34 +0200

    Implement base styles for apod component

--- components/nasa/Apod.vue
+++ components/nasa/Apod.vue
@@ -1,10 +1,27 @@
 <template>
   <div class="apod">
-    {{ $moment(apodData.date).format('DD-MM-YYYY') }}
-    {{ apodData.copyright }}
-    {{ apodData.title }}
-    <img :src="apodData.url"/>
-    <div v-html="apodData.explanation"></div>
+    <h2 class="apod__title mb-3">Astronomy Picture of the Day</h2>
+    <div class="apod__date fw-bold">{{ $moment(apodData.date).format('DD MMM YYYY') }}</div>
+    <h3 class="apod__picture-title display-6 mb-3">{{ apodData.title }}</h3>
+    <template v-if="apodData.media_type == 'image'">
+      <picture class="apod__picture">
+        <img :src="apodData.url"/>
+      </picture>
+    </template>
+    <template v-else>
+      <div class="ratio ratio-16x9">
+        <iframe :src="apodData.url" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen>
+        </iframe>
+      </div>
+    </template>
+    
+    <div v-if="apodData.copyright" 
+         class="apod__copyright">&copy; <em>{{ apodData.copyright }}</em></div>
+    <div class="apod__explanation mt-3" v-html="apodData.explanation"></div>
   </div>
 </template>
 
@@ -16,7 +33,8 @@ export default {
   data() {
     return {
       apodData: [],
-      date: this.$moment().format('YYYY-MM-DD')
+      //date: this.$moment().format('YYYY-MM-DD')
+      date: '2022-02-10' // video
     }
   },
   async fetch() {
